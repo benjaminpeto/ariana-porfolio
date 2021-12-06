08d477d6238fec0d6b03d85684355b92fec4de94
Benji <benjaminpeto23@gmail.com>
2021-12-06 14:16:06 +0100

    added navigation bar, installed react-router, implemented styling to navbar and added assets to folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+/* import { Routes, Route, Link } from "react-router-dom"; */
+
+import Navigation from "./Components/Navigation/Navigation.components";
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Navigation />
     </div>
   );
 }
