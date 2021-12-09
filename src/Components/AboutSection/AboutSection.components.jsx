ee85a9e673e6f9aa0d71fe52484cd8202659432d
Benji <benjaminpeto23@gmail.com>
2021-12-09 18:49:12 +0100

    started to build second section of hompage, added new font and background

--- src/Components/AboutSection/AboutSection.components.jsx
+++ src/Components/AboutSection/AboutSection.components.jsx
@@ -5,7 +5,29 @@ import './AboutSection.styles.scss';
 function AboutSection() {
   return (
     <section className='about-wrapper'>
-    
+
+        <h1>Hola!</h1>
+        <div className="paragraph-wrapper">
+          <div>
+            <p>
+              I'm Ariana, a UX/UI | graphic designer who is passionate about providing interactive experiences and identities to forward thinking brands and companies worldwide.
+              Having gained an architectural design based degree, my career saw a natural progression from merchandising, spacial design, enterprise management to graphic and UX/UI design.
+            </p>
+            <p>
+              For the past nine years, I've worked on various design projects including, package design, outdoor and print advertisements, branding, illustrations and most recently UX/UI design.
+            </p>
+          </div>
+
+          <div>
+            <p>
+              Using mostly Adobe Creative Suite alongside Figma, I have implemented successful design solutions for brands such as Eat at Sicily, Baker & Spice, Philpotts, Patisserie Valerie, R2 Hotels and Rubys of London.
+            </p>
+            <p>
+              I have an end-to-end understanding of project development from concept to solution, and from pitch to discussed implementation. I enjoy challenges and working to targets, planning ahead, problem solving and quick thinking. Creatively, I enjoy brainstorming and innovative ideas that exploit target market opportunities using advertising strategies that impact consumer buying trends. 
+            </p>
+          </div>
+        </div>
+
     </section>
   );
 }
