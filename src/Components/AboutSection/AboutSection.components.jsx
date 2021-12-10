3cba3bf1dfaac6c19487038b1b0f20502d74175d
Benji <benjaminpeto23@gmail.com>
2021-12-10 13:25:49 +0100

    implemented responsiveness, added CV download button, fixed styling, added fake pdf

--- src/Components/AboutSection/AboutSection.components.jsx
+++ src/Components/AboutSection/AboutSection.components.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import resume from '../../data/ariana_scalzo_cv.pdf'
+
 import './AboutSection.styles.scss';
 
 function AboutSection() {
@@ -13,7 +15,7 @@ function AboutSection() {
               I'm Ariana, a UX/UI | graphic designer who is passionate about providing interactive experiences and identities to forward thinking brands and companies worldwide.
               Having gained an architectural design based degree, my career saw a natural progression from merchandising, spacial design, enterprise management to graphic and UX/UI design.
             </p>
-            <p>
+            <p className="odd-paragraph">
               For the past nine years, I've worked on various design projects including, package design, outdoor and print advertisements, branding, illustrations and most recently UX/UI design.
             </p>
           </div>
@@ -26,6 +28,9 @@ function AboutSection() {
               I have an end-to-end understanding of project development from concept to solution, and from pitch to discussed implementation. I enjoy challenges and working to targets, planning ahead, problem solving and quick thinking. Creatively, I enjoy brainstorming and innovative ideas that exploit target market opportunities using advertising strategies that impact consumer buying trends. 
             </p>
           </div>
+        </div>
+        <div className="button-wrapper">
+          <a className='resume-btn' href={resume} download="ariana_scalzo_cv.pdf">RESUME</a>
         </div>
 
     </section>
