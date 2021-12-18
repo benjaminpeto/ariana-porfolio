9218bd301550310b0303c35d9dc66eb539a09176
Benji <benjaminpeto23@gmail.com>
2021-12-18 14:19:01 +0100

    added image slider component, finished styling, added hover effect to navigation bar, fixed positions in portfolio page

--- src/Components/SlideShow/SlideShow.components.jsx
+++ src/Components/SlideShow/SlideShow.components.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-import './SlideShow.styles.scss';
+import img1 from '../../assets/Peru coffee.webp';
+import img2 from '../../assets/coco moon hair spray.webp';
+import img3 from '../../assets/coconut pool.webp';
+import img4 from '../../assets/notebook_.webp';
 
+import './SlideShow.styles.scss';
 
 const SlideShow = () => {
 
   return(
     <div className="slider">
-      
+      <img src={img1} alt="" />
+      <img src={img2} alt="" />
+      <img src={img3} alt="" />
+      <img src={img4} alt="" />
     </div>
   );
 }
