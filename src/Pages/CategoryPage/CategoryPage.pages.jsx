3e02f1e9ed5e252522447bc6a1ba3ab80aeb07d1
Benji <benjaminpeto23@gmail.com>
2021-12-20 18:39:15 +0100

    implemented react-3d-craousel

--- src/Pages/CategoryPage/CategoryPage.pages.jsx
+++ src/Pages/CategoryPage/CategoryPage.pages.jsx
@@ -1,20 +1,39 @@
 import React from "react";
 
+import { Carousel } from '3d-react-carousal';
+
 /* import { Link } from "react-router-dom"; */
+
+import baker from '../../assets/carousel-baker-&-spice.webp';
+import benji from '../../assets/carousel-benjamin-peto.webp';
+import coco from '../../assets/carousel-coco-moon.webp';
 
 import './CategoryPage.styles.scss';
 
-function CategoryPage() {
-  return (
-    <div className="category-page">
-      <div className="carousel-wrapper">
+class CategoryPage extends React.Component {
 
-        <div className="navigation-wrapper">
-        
-        </div>
+  render() {
+    let slides = [
+      <div className="image-wrapper">
+        <img  src={baker} alt="1" height={700} />
+        <h1>baker</h1>
+      </div>,
+      <div className="image-wrapper">
+        <img  src={benji} alt="1" height={700} />
+        <h1>benji</h1>
+      </div>,
+      <div className="image-wrapper">
+        <img  src={coco} alt="1" height={700} />
+        <h1>coco</h1>
       </div>
-    </div>
-  );
+    ];
+
+    return (
+      <div className="category-wrapper">
+        <Carousel slides={slides} />
+      </div>
+    );
+  }
 }
 
 export default CategoryPage;
