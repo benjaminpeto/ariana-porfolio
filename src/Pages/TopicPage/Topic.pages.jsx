6ad6e1fe426f3df24cf050c6d473f98d7e8253fd
Benji <benjaminpeto23@gmail.com>
2022-01-26 14:35:27 +0200

    added data to topics, need to figure out routing different data...

--- src/Pages/TopicPage/Topic.pages.jsx
+++ src/Pages/TopicPage/Topic.pages.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import { topicData } from './topicData';
+
 function TopicPage() {
   let { topic } = useParams();
   return (
     <div>
-      this is the topic of {topic}
+      <h1>{topic.toUpperCase().replace(/-/g , ' ')}</h1>
+      {
+        topicData.map((data) => (
+          <div key={data.id}>
+            <h3>{data.title}</h3>
+            <p>{data.description}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
