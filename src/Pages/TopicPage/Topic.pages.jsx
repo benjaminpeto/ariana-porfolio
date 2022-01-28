f581f069b9a0bf5234dc13939da4fc7349d568ee
Benji <benjaminpeto23@gmail.com>
2022-01-28 18:18:07 +0200

    doing something wrong, but got the response what i need

--- src/Pages/TopicPage/Topic.pages.jsx
+++ src/Pages/TopicPage/Topic.pages.jsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { topicData } from './topicData';
 
-function TopicPage() {
-  let { topic } = useParams();
+const TopicPage = (props) => {
+  const [topic, setTopic] = useState([topicData]);
+  let params = useParams();
+
+  const getTopic = () =>{
+    if(params.topic){
+      const res = params.topic;
+      let data;
+      [...topic].forEach(item => {
+        for( const key in item ) {
+          if(res === item[key].id){
+            data = item[key];
+          }
+        }
+      })
+      console.log('found DaTA---->', data);
+      setTopic(data);
+    }
+  };
+  
+  useEffect(() => {
+    getTopic(props);
+  });
+  
   return (
-    <div>
-      <h1>{topic.toUpperCase().replace(/-/g , ' ')}</h1>
-      {
-        topicData.map((data) => (
-          <div key={data.id}>
-            <h3>{data.title}</h3>
-            <p>{data.description}</p>
-          </div>
-        ))
-      }
+    <div {...props} params={params}>
+    {
+      topic.map((item, idx) => (
+        <div key={idx}>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+        </div>
+      ))
+    }
     </div>
   );
 }
