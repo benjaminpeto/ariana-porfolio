98e43ea36761ab43df926691b2289b3976575315
Benji <benjaminpeto23@gmail.com>
2022-02-13 17:32:25 +0200

    added optimized images, and content to the potfolio page, installed validator from npm, made a basic validation for the contact page, only styles need to be add

--- src/Pages/ContactPage/Contact.pages.jsx
+++ src/Pages/ContactPage/Contact.pages.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import emailjs from 'emailjs-com';
+import isEmail from "validator/lib/isEmail";
 
 import Modal from '../../Components/Modal/Modal.components';
 
@@ -9,17 +10,43 @@ const Contact = (props) => {
   const form = useRef();
 
   const [modalOpen, setModalOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const onNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const onEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_ID', 'template_ID', form.current, 'user_ID')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset();
+    console.log('email---->', email);
+    
+    if (!name || !email || !message) {
+      alert('You must fill out every field!');
+    }
+    else if (!isEmail(email)) {
+      alert("Email needs to be a valid address.");
+    }
+    else {
+      emailjs.sendForm('service_ID', 'template_ID', form.current, 'user_ID')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+        setModalOpen(true);
+    }
   };
 
   return (
@@ -27,22 +54,24 @@ const Contact = (props) => {
 
       <h1>Let's work together!</h1>
 
-      <form ref={form}
+      <form
+        ref={form}
         onSubmit={(e) => {
           sendEmail(e);
-          setModalOpen(true);
-        }}>
+        }}
+        noValidate //removing default validation of HTML
+      >
 
         <div className='name-email'>
           <label>Name</label>
-          <input type="text" name="user_name" placeholder="Name" />
+          <input type="text" name="user_name" placeholder="Name" onChange={onNameChange} />
           <label>Email</label>
-          <input type="email" name="user_email" placeholder="Email" />
+          <input type="email" name="user_email" placeholder="Email" onChange={onEmailChange} />
         </div>
 
         <div className='message-div'>
           <label>Message</label>
-          <textarea name="message" placeholder="Write your message..." />
+          <textarea name="message" placeholder="Write your message..." onChange={onMessageChange} />
           <input type="submit" value="SUBMIT" />
         </div>
 
--- src/data/topicData.js
+++ src/data/topicData.js
@@ -1,8 +1,71 @@
-import coco1 from '../assets/coco moon branding copy.webp';
-import coco2 from '../assets/coco moon facial niht oils.webp';
-import coco3 from '../assets/coco moon hair spray.webp';
-import coco4 from '../assets/coco moon loo.webp';
-import coco5 from '../assets/coco pool ipad_.webp';
+// COCO MOON
+
+import coco1 from '../assets/cocomoon/coco moon loo.webp';
+import coco2 from '../assets/cocomoon/coco-moon-4k.gif';
+import coco3 from '../assets/cocomoon/coco moon facial niht oils.webp';
+import coco4 from '../assets/cocomoon/coco moon hair spray.webp';
+import coco5 from '../assets/cocomoon/face mask coco moon.webp';
+import coco6 from '../assets/cocomoon/coco moon branding copy.webp';
+import coco7 from '../assets/cocomoon/2631.webp';
+
+// SICILY
+
+import sicily1 from '../assets/sicily/sicily-logo-gif.gif';
+import sicily2 from '../assets/sicily/pizza box sicily.webp';
+import sicily3 from '../assets/sicily/pattern 2.webp';
+import sicily4 from '../assets/sicily/pattern 4.webp';
+import sicily5 from '../assets/sicily/pattern 3.webp';
+import sicily6 from '../assets/sicily/pattern 1.webp';
+import sicily7 from '../assets/sicily/salad box sicily.webp';
+import sicily8 from '../assets/sicily/pasta box sicily.webp';
+import sicily9 from '../assets/sicily/patone sicily.webp';
+import sicily10 from '../assets/sicily/stickers sicily.webp';
+import sicily11 from '../assets/sicily/sicily-logo-2.webp';
+import sicily12 from '../assets/sicily/coffee cup sicily.webp';
+import sicily13 from '../assets/sicily/napkin sicily.webp';
+
+// Baker&Spice
+
+import baker1 from '../assets/baker-spice/Peru coffee.webp';
+import baker2 from '../assets/baker-spice/4680654.webp';
+import baker3 from '../assets/baker-spice/b&S coffee photo.webp';
+import baker4 from '../assets/baker-spice/Vietnam coffee.webp';
+import baker5 from '../assets/baker-spice/foil-05.webp';
+import baker6 from '../assets/baker-spice/Brazil coffee_.webp';
+
+// Benjamin
+
+import benji1 from '../assets/benji/Process_-BP-LOGO-2.gif';
+import benji2 from '../assets/benji/bp mock up.webp';
+import benji3 from '../assets/benji/pb logo.webp';
+import benji4 from '../assets/benji/Stationary_Mockup_005.webp';
+
+// Day Dreaming
+
+import daydream1 from '../assets/daydreaming/notebook_.webp';
+import daydream2 from '../assets/daydreaming/ipad pro psd mockup.webp';
+import daydream3 from '../assets/daydreaming/daydreamingflowers.webp';
+import daydream4 from '../assets/daydreaming/daydreamingmasks.webp';
+import daydream5 from '../assets/daydreaming/daydreamingpalms.webp';
+
+// Saladita Studio
+
+import saladita1 from '../assets/saladitastudio/coconut pool.webp';
+import saladita2 from '../assets/saladitastudio/coco pool ipad_.webp';
+import saladita3 from '../assets/saladitastudio/flower tshirt.webp';
+import saladita4 from '../assets/saladitastudio/hibsicus ipad_.webp';
+import saladita5 from '../assets/saladitastudio/drowning.webp';
+import saladita6 from '../assets/saladitastudio/duck dive ipad.webp';
+import saladita7 from '../assets/saladitastudio/longboarders_.webp';
+import saladita8 from '../assets/saladitastudio/longboard shell_.webp';
+import saladita9 from '../assets/saladitastudio/rainbow_.webp';
+
+// Surf&Soul
+
+import surf1 from '../assets/surfsoul/surfnsoul.gif';
+import surf2 from '../assets/surfsoul/soulnsurf-series.webp';
+import surf3 from '../assets/surfsoul/surf-skate-4k.gif';
+import surf4 from '../assets/surfsoul/surfskate-strip.webp';
 
 
 export const topicData = [
@@ -12,7 +75,7 @@ export const topicData = [
     path: '/portfolio/branding/coco-moon',
     subtitle: 'branding, packaging design & concept design',
     description: "Coco Moon is a natural and organic skincare brand, committed to setting new standards in the world of self care, using only the finest ingredients in their range. Coco Moon strives to enforce sustainable and ethical practices across all people, animals and land evolved. Valuing the brand's mission, all visual points from the logo, identity system to sustainable packaging solutions using innovative materials, speak a univeral language of elevated design, sustainability and self-expression. Coco Moon is inspired by the moons lunar phases and strives off the energy it gives off to the earth. The brand tailors its product to each lunar phase to how its monthly journey effects our personalities, outlooks and lives. Coco Moon encourages awareness of the moons four main stages and four intermediate phases,  awakening a new understaning of ourselves and its power.",
-    images: [coco1, coco2, coco3, coco4, coco5]
+    images: [coco1, coco2, coco3, coco4, coco5, coco6, coco7]
   },
   {
     id: '102',
@@ -20,7 +83,7 @@ export const topicData = [
     path: '/portfolio/branding/sicily-restaurant',
     subtitle: 'branding & graphic design',
     description: "Overarching brand identity design for an Italian restaurant in the heart of London. The brand took a refresh effect by introducing more vibrant colours that had a strong connection with the attributes of the brand and gave that instant impression of offering an authentic Italian experience. Now the brand gives off a more playful and modern typography and identity system can be seen applied throughouty all of Sicily's collateral. The colours and pattern drawn as organic shapes come from typical italian ingredients used in its cuisine; Pizza, tomatoes, basil, lemons, oranges.",
-    images: []
+    images: [sicily1, sicily2, sicily3, sicily4, sicily5, sicily6, sicily7, sicily8, sicily9, sicily10, sicily11, sicily12, sicily13]
   },
   {
     id: '103',
@@ -28,7 +91,7 @@ export const topicData = [
     path: '/portfolio/branding/baker-&-spice',
     subtitle: 'branding, illustration & package design',
     description: "I was tasked to design coffee bag packaging for a coffee shop brand Baker & Spice in the UK. It seemed only natural to incorporate the coffee's origins and culture within that design through illustrations of a scene that ties in all of the country's elements in harmony. I chose iconic sceneries from each country that showcase its natural bright colours. The chosen color palette gives it that fresh and modern feel, while the silky-finish makes the packaging look luxurious.",
-    images: []
+    images: [baker1, baker2, baker3, baker4, baker5, baker6]
   },
   {
     id: '104',
@@ -36,7 +99,7 @@ export const topicData = [
     path: '/portfolio/branding/benjamin-peto',
     subtitle: 'branding, logo design & graphic design',
     description: "The brief for this project was to create a logo and brand identity for a Web developer. I designed Benjamin's logo whilst also representing his purpose as a Web developer by taking common characters from coding language and translating these into the clients initials 'B.P'. I took great emphasis on the logo and created a better connection with his role by creating a fun animation that showcases his logo first and reveals how his initials are also blended into the design. My aim was to have individuals recognise exactly what form of service Benjamin offers in a quick, succinct fashion. Crafting an image that represents his purpose in a glance allows viewers to understand and become interested much quicker.",
-    images: []
+    images: [benji1, benji2, benji3, benji4]
   },
   {
     id: '105',
@@ -44,7 +107,7 @@ export const topicData = [
     path: '/portfolio/illustrations/day-dreaming',
     subtitle: 'illustration & product design',
     description: "This illustration series is for a collection of notebooks. I tried to capture the mood with the color palette as much as possible where my aim was to reflect the tone for the concept 'day dreaming'. I primarily sketched all my designs but I never shy away from using the computer as a tool to manipulate and enhance my paintings to make them better. The final result is translated through digital vector art, where I seperated all elements into layers. This made it easier to copy certain elements from one drawing to another and to edit them where needed.",
-    images: []
+    images: [daydream1, daydream2, daydream3, daydream4, daydream5]
   },
   {
     id: '106',
@@ -52,7 +115,7 @@ export const topicData = [
     path: '/portfolio/illustrations/saladita-studio',
     subtitle: 'illustration & product design',
     description: "Raised in the Canary Islands by a family with a strong passion for surfing, Saladita studio is a personal project where I  share my endless love for the Atlantic ocean. I translate this through a series of illustrations of geometric figures, retro surf iconography and classic surf movements you can find in the water. Illustrating my simple life by the ocean and what it represents is an expression of what the ocean and its waves have taught me. For personal use and those of friends and family these digital drawings have been printed on products such as tote bags, t-shirts, notebooks and frames.  ",
-    images: []
+    images: [saladita1, saladita2, saladita3, saladita4, saladita5, saladita6, saladita7, saladita8, saladita9]
   },
   {
     id: '107',
@@ -60,6 +123,6 @@ export const topicData = [
     path: '/portfolio/illustrations/surf-&-soul',
     subtitle: 'illustration & animation',
     description: "Raised in the Canary Islands by a family with a strong passion for surfing, Saladita studio is a personal project where I  share my endless love for surf and concrete surfing. I translate this through a series of illustrations of geometric figures, retro surf iconography and frames of classic surf movements to create a fun looped GIF.",
-    images: []
+    images: [surf1, surf2, surf3, surf4]
   },
 ]
