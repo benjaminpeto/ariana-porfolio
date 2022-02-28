1556e172a693d35666fcf398a6e59f9d1fffad2f
Benji <benjaminpeto23@gmail.com>
2022-02-28 17:26:39 +0300

    changed bg picture on main page, edited styles on multiple component, added ux-ui page and its styles, started to build up loading page, added useEffect to remove that from DOM after loaded, currenlty fine tuning loading page style

--- src/Components/WelcomeSection/WelcomeSection.components.jsx
+++ src/Components/WelcomeSection/WelcomeSection.components.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import PageLoadingAnimation from "../PageLoadingAnimation/PageLoadingAnimation.components";
 
 import './WelcomeSection.styles.scss';
 
-function WelcomeSection() {
+const WelcomeSection = (props) => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(false);
+    }, 4000);
+  }, [props]);
+
   return (
     <>
       <div className='main-wrapper'>
-        <PageLoadingAnimation />
+      {
+        visible && <PageLoadingAnimation />
+      }
         <h1>Ariana Scalzo</h1>
       </div>
     </>
--- src/Pages/WebDesignPage/WebDesign.pages.jsx
+++ src/Pages/WebDesignPage/WebDesign.pages.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+
+import uxui from "../../assets/uxui.png";
 
 const WebDesign = (props) => {
-  return(
-    <div style={{padding: '100px 50px', fontSize: '40px'}}>
-      UX_UI Design of this page
-    </div>
-  );
-}
+	return (
+		<div className="topic-page-container">
+			<div>
+				<h1>UX-UI</h1>
+				<h3>blabla</h3>
+				<p>Aute aliqua ut proident aliqua Lorem. Eu enim laboris est sint ad ea ad aliqua incididunt. Laboris incididunt nostrud elit occaecat incididunt reprehenderit et. Exercitation qui anim ut fugiat pariatur.</p>
+				<img alt="" src={uxui} className="responsive" />
+			</div>
+		</div>
+	);
+};
 
 export default WebDesign;
