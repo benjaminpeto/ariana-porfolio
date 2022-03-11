640de72f200b0038cd528aa734031b9d7281f445
Benji <benjaminpeto23@gmail.com>
2022-03-11 16:35:06 +0300

    fixed another design issues and background picture on mobile device, added logos for manifest.json, install google analitics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import GA4React from "ga-4-react";
+
 import { BrowserRouter } from "react-router-dom";
 
 import './Fonts/Solar Vesta Serif.ttf';
@@ -9,12 +11,22 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+const ga4react = new GA4React(process.env.REACT_APP_GA4_TRACKING_ID);
+
+(async _ => {
+
+  await ga4react.initialize()
+  .then(res => console.log("Analytics Success."))
+  .catch(err => console.log("Analytics Failure."))
+  .finally(() => {
+    ReactDOM.render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      document.getElementById('root')
+    );
+  });
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
